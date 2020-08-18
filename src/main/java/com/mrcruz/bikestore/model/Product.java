2a25af8f803370687e119c46f6a59e0eb40fcb09
ewdemy <ewdemy@hotmail.com>
2020-08-18 20:51:53 -0300

    create entities product, brand and category

---
+++ src/main/java/com/mrcruz/bikestore/model/Product.java
@@ -0,0 +1,69 @@
+package com.mrcruz.bikestore.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="products")
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="product_id")
+	private Long id;
+	
+	@Column(name="product_name")
+	private String name;
+	@Column(name="model_year")
+	private int modelYear;
+	@Column(name="list_price")
+	private double listPrice;
+	@Column(name="brand_id")
+	private Brand brand;
+	@Column(name="category_id")
+	private Category category;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getModelYear() {
+		return modelYear;
+	}
+	public void setModelYear(int modelYear) {
+		this.modelYear = modelYear;
+	}
+	public double getListPrice() {
+		return listPrice;
+	}
+	public void setListPrice(double listPrice) {
+		this.listPrice = listPrice;
+	}
+	public Brand getBrand() {
+		return brand;
+	}
+	public void setBrand(Brand brand) {
+		this.brand = brand;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
+	
+	
+}
